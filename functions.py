35920316b98f4f4e351add890832958f94a47bea
Felix Filbert <felixfilbert33@gmail.com>
2026-04-22 21:15:05 +0700

    functions debug

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def get_todos(filepath=FILEPATH):
 
 def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
-        file.writelines(todos_arg) + "\n"
+        file.writelines(todos_arg)
 
 if __name__ == "__main__":
     print("Hello")
